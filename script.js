4d6692039fbb167b0c1ac6b4f1c33b19578c8fab
ByronFraser <byronfraser@live.co.za>
2024-12-29 11:33:49 +0700

    ui_complete

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
+let cScore = 0;
+let hScore = 0;
+let tCounter = 0;
+        let counter = 0;
 
+const computerText = document.getElementById('computerChoice');
+const humanText = document.getElementById('humanChoice');
+const scoreText = document.getElementById('scoreTracker');
 
+function getWinner(hChoice){
 
-
-
-function getWinner(getComputerChoice, getHumanChoice){
-
-        let cScore = 0;
-        let hScore = 0;
-
-        for (let i = 1; i < 5; i++){
-        let hChoice = prompt('Choose: Rock, Paper or Scissors');
-        console.log('Your Choice: ' + hChoice);
+        console.log("You : " + hChoice);
         
         cChoice = Math.random()*10*(1/3);
         if ((cChoice <=1)){
@@ -18,44 +17,55 @@ function getWinner(getComputerChoice, getHumanChoice){
         }else if ((cChoice<2)&&(cChoice>1)){
             cChoice = 'Paper';
         }else cChoice = 'Scissors';
-        console.log('Computer :' + cChoice);
 
-        
+        humanText.textContent = 'You : ' + hChoice;
+        computerText.textContent = 'Computer : ' + cChoice;
+        console.log('Computer : ' + cChoice);
+
         
         if ((cChoice == "Paper")&&(hChoice == "Paper")){
             console.log('Tie');
+            counter++;
+            tCounter++;
         } else if ((cChoice == "Rock")&&(hChoice == "Rock")){
             console.log('Tie');
+            counter++;
+            tCounter++;
         }else if ((cChoice == "Scissors")&&(hChoice == "Scissors")){
             console.log('Tie');
+            counter++;
+            tCounter++;
         }else if ((cChoice == "Paper")&&(hChoice == "Rock")){
             console.log('You Loose');
             cScore++;
+            counter++;
         }else if ((cChoice == "Paper")&&(hChoice == "Scissors")){
             console.log('You Win');
             hScore++;
+            counter++;
         }else if ((cChoice == "Scissors")&&(hChoice == "Rock")){
             console.log('You Win');
             hScore++;
+            counter++;
         }else if ((cChoice == "Scissors")&&(hChoice == "Paper")){
             console.log('You Loose');
             cScore++;
+            counter++;
         }else if ((cChoice == "Rock")&&(hChoice == "Paper")){
             console.log('You Win');
             hScore++;
+            counter++;
         }else if ((cChoice == "Rock")&&(hChoice == "Scissors")){
             console.log('You Loose');
             cScore++;
+            counter++;
         } else
         console.log('Incorrect Input');
-    }
+    
     console.log('Computer Score:' + cScore + ' Your Score:' + hScore);
-    if (cScore>hScore){
-        console.log('Computer is the Winner')
-    }else if (hScore>cScore){
-        console.log('You are the winner')
-    } else {
-        console.log('It is a Tie');
+    
+    scoreText.textContent = "Score -- " + "Computer: " + cScore + "  You: " + hScore;
+    if (counter == 5) {
+        alert('Final Score : ' + 'Wins ' + hScore + "  Losses " + cScore + '  Ties ' + tCounter);
     }
 }
-getWinner()
